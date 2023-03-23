e546cd7b797cde4fa1a3a0153110c6281bda9790
dahlia <wlstnrdahlia@gmail.com>
2023-03-23 22:37:39 +0900

    feat : Nav 컴포넌트

--- github/src/App.js
+++ github/src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
-import axios from 'axios';
+import Nav from './Nav/Nav';
+import SearchBar from './SearchBar/SearchBar';
+import UserContent from './UserContent/UserContent';
 
 function App(input) {
 
-  const handleChange = (input) => {
-    let value = input;
+  // const handleChange = (inputValue) => {
+  //   onChange={(e) => handleChange(e.target.value)}
+  //   let value = inputValue;
 
-    axios({
-      url: `https://api.github.com/users/${value}`,
-      method : 'GET',
+  //   axios({
+  //     url: `https://api.github.com/users/${value}`,
+  //     method : 'GET',
       
-    }).then((res) => {
-      console.log(res.data);
-    }).catch((error)=> {
-      console.log(error);
-    })
-  }
+  //   }).then((res) => {
+  //     console.log(res.data);
+  //   }).catch((error)=> {
+  //     console.log(error);
+  //   })
+  // }
   return (
-    <div className="App">
-     <input onChange={(e) => handleChange(e.target.value)} type='text'/>
-    </div>
+    <>
+      <Nav />
+      <SearchBar />
+      <UserContent />
+    </>
   );
 }
 
