8eed0f6cbedc9c20c07b3faa048b4580636f44b1
dahlia <wlstnrdahlia@gmail.com>
2023-03-23 22:53:00 +0900

    feat : SearchBar 컴포넌트

--- github/src/SearchBar/SearchBar.jsx
+++ github/src/SearchBar/SearchBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from './SearchBar.module.css';
+import { FiSearch } from 'react-icons/fi';
 
 export default function SearchBar() {
   return (
-    <div>
-      
+    <div className={styles.box}>
+      <FiSearch className={styles.icon}/>
+      <input type='text' placeholder='Search Github username...' className={styles.searchBar}/>
     </div>
-  );
+  )
 }
 
