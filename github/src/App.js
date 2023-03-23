0eb03d29536180ee66c79b0107163cd323f09445
dahlia <wlstnrdahlia@gmail.com>
2023-03-23 21:28:29 +0900

    feat : input value 변경 시 유저 정보 받아오는 기능

--- github/src/App.js
+++ github/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+function App(input) {
+
+  const handleChange = (input) => {
+    let value = input;
+
+    axios({
+      url: `https://api.github.com/users/${value}`,
+      method : 'GET',
+      
+    }).then((res) => {
+      console.log(res.data);
+    }).catch((error)=> {
+      console.log(error);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input onChange={(e) => handleChange(e.target.value)} type='text'/>
     </div>
   );
 }
