149c96bdf7f2a46a15eb3422cd34ba9d17b8dc96
dahlia <wlstnrdahlia@gmail.com>
2023-03-24 11:14:24 +0900

    폴더 위치 수정

--- github/src/UserContent/UserContent.jsx
+++ github/src/UserContent/UserContent.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import data from '../data';
+import styles from './UserContent.module.css';
 
 export default function UserContent() {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <img src={data.avatar_url} className={styles.img} />
+      <div className={styles.userBox}>
+        <h1>{data.login}</h1>
+        <a href={data.html_url} className={styles.userPage}>@{data.login}</a>
+        <div className={styles.accountInfo}>
+          <div classNmae={styles.box}>
+            <p>repos</p>
+            <p className={styles.num}>{data.public_repos}</p>
+          </div>
+          <div classNmae={styles.box}>
+            <p>followings</p>
+            <p className={styles.num}>{data.public_repos}</p>
+          </div>
+          <div classNmae={styles.box}>
+            <p>followers</p>
+            <p className={styles.num}>{data.public_repos}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
