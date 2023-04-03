4b7ff7f6c55353077c85c8f4463711d975d2c5c2
dahlia <wlstnrdahlia@gmail.com>
2023-04-04 00:00:52 +0900

    feat: Categories, Button 컴포넌트

--- news/src/components/Button/Button.jsx
+++ news/src/components/Button/Button.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Button.css';
 
-export default function Button() {
+export default function Button({ value }) {
+  // 각각의 button 컴포넌트가 onclick 되었을때 그 값을 map돌면서 해당 idx만 classList add 해주는 방식으로 가져가면 될듯
   return (
-    <div>
-      
-    </div>
+    <button 
+      className='btn px-px px-4 py-py py-2 rounded-xl' 
+      type='button'>
+        {value}
+    </button>
   );
 }
 
--- news/src/components/Categories/Categories.jsx
+++ news/src/components/Categories/Categories.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Button from '../Button/Button';
 
 export default function Categories() {
+  const categories = [
+    'general',
+    'business',
+    'technology',
+    'science',
+    'entertainment',
+    'health',
+    'sports'
+  ]
+
   return (
-    <div>
-      
+    <div className='mx-5 category flex gap-x-2 mt-10'>
+      { categories.map((category, idx) => <Button key={idx} value={category}/>) }
     </div>
   );
 }
